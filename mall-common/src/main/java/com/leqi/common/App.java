d2067ebf170ab102bffe6a58f8e1a9400d4ea8f1
e <810638884@qq.com>
2019-08-28 11:31:50 +0800

    0828

---
+++ mall-common/src/main/java/com/leqi/common/App.java
@@ -0,0 +1,13 @@
+package com.leqi.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
