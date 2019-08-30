4050aee79ddf3021760c668bf3c8930b6be39ed2
e <810638884@qq.com>
2019-08-30 17:27:00 +0800

    加入登录页面

--- vue-mall-admin/src/views/Login.vue
+++ vue-mall-admin/src/views/Login.vue
@@ -1,28 +1,38 @@
 <template>
+  <div class="container">
     <div>
-        <el-row>
-            <el-col :span="6">
-                <div class="grid-content bg-purple-dark">1</div>
+      <el-row>
+            <el-col :span="8">
+                <div class="grid-content bg-purple-dark">&nbsp;</div>
             </el-col>
-            <el-col :span="6">
-                <div class="grid-content bg-purple-dark">1</div>
+            <el-col :span="8">
+                <div class="grid-content bg-purple-dark">
+                    <h3>欢迎登录</h3>
+                    <el-form ref="form" :model="form" label-width="80px">
+                </div>
             </el-col>
-            <el-col :span="6">
-                <div class="grid-content bg-purple-dark">1</div>
+            <el-col :span="8">
+                <div class="grid-content bg-purple-dark">&nbsp;</div>
             </el-col>
-            <el-col :span="6">
-                <div class="grid-content bg-purple-dark">1</div>
-            </el-col>
-        </el-row>
+           
+      </el-row>
     </div>
+  </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
 <style lang="scss" scoped>
-.grid-content{
-    background-color: pink
+.grid-content {
+  background-color: pi;
+}
+
+.container {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  background-image: url("../assets/background_img.jpg");
 }
 </style>
