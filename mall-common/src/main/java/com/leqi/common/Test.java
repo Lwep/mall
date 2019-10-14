b0e595d89fd9a8f5d742c237c99e206d4ee8e54b
e <810638884@qq.com>
2019-10-14 17:18:30 +0800

    接入jenkins

--- mall-common/src/main/java/com/leqi/common/Test.java
+++ mall-common/src/main/java/com/leqi/common/Test.java
@@ -1,63 +1,63 @@
-package com.leqi.common;
-
-import java.lang.reflect.Array;
-import java.util.*;
-
-/**
- * @author lwep
- * @dareTime 2019/9/6 19:14
- */
-public class Test {
-    //static int x=10;
-    //static { x+=5;}
-
-    public static void main(String args[]) {
-
-        int a  = 0;
-        short b= 1;
-        b = (short) (b+1);
-        System.out.println(a);
-//        boolean bool = true;
-//        System.out.println(bool);
-//        if(bool = false){
-//            System.out.println("a"+bool);
-//        }
-//        else if(bool)
-//        {
+//package com.leqi.common;
 //
-//            System.out.println("b"+bool);
-//        }
-//        else if(!bool){
-//            System.out.println("c"+bool);
-//        }
-//        else
-//        {
-//            System.out.println("d"+bool);
-//        }
-//        System.out.println(Math.round(-11.5));
-//        int array[] = {3, 1, 2, 3, 3, 3, 4, 1, 5, 2, 6, 8};
-//        String a = "AA123ABBDFD12352123AC";
-//        Set set = new LinkedHashSet<>();
-//        char[] chars = a.toCharArray();
-//        for(int i = 0 ; i < a.length() ; i ++){
-//            set.add(chars[i]);
-//        }
-//             System.out.println(set.toString());
-//        float f = 1.3;
-//        int a = 12*31;
-//        int b = 540;
-//        int i =0;
-//        int x = a;
-//        while(a>0){
-//            a = a-31+15;
-//            x = x+15;
-//            b -= 15;
-//            i++;
-//            System.out.println(i+"+++"+a+" +++"+b+"aaaaaaa"+x);
-
-
-
-
-    }
-
-}
+//import java.lang.reflect.Array;
+//import java.util.*;
+//
+///**
+// * @author lwep
+// * @dareTime 2019/9/6 19:14
+// */
+//public class Test {
+//    //static int x=10;
+//    //static { x+=5;}
+//
+//    public static void main(String args[]) {
+//
+//        int a  = 0;
+//        short b= 1;
+//        b = (short) (b+1);
+//        System.out.println(a);
+////        boolean bool = true;
+////        System.out.println(bool);
+////        if(bool = false){
+////            System.out.println("a"+bool);
+////        }
+////        else if(bool)
+////        {
+////
+////            System.out.println("b"+bool);
+////        }
+////        else if(!bool){
+////            System.out.println("c"+bool);
+////        }
+////        else
+////        {
+////            System.out.println("d"+bool);
+////        }
+////        System.out.println(Math.round(-11.5));
+////        int array[] = {3, 1, 2, 3, 3, 3, 4, 1, 5, 2, 6, 8};
+////        String a = "AA123ABBDFD12352123AC";
+////        Set set = new LinkedHashSet<>();
+////        char[] chars = a.toCharArray();
+////        for(int i = 0 ; i < a.length() ; i ++){
+////            set.add(chars[i]);
+////        }
+////             System.out.println(set.toString());
+////        float f = 1.3;
+////        int a = 12*31;
+////        int b = 540;
+////        int i =0;
+////        int x = a;
+////        while(a>0){
+////            a = a-31+15;
+////            x = x+15;
+////            b -= 15;
+////            i++;
+////            System.out.println(i+"+++"+a+" +++"+b+"aaaaaaa"+x);
+//
+//
+//
+//
+//    }
+//
+//}
